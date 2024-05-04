a82e6f14e223d4a1c52597fc90cea95638c02423
Parth Nanda <parthnanda002@gmail.com>
2024-05-04 20:45:06 +0530

    Added axios to fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import "./App.css";
-import JobsList from './components/JobList';
-import jobs from './components/Jobs'; // the mock data file
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import JobList from './components/JobList';
 
-const App = () => {
+function App() {
+  const [jobData, setJobData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      axios.post('https://api.weekday.technology/adhoc/getSampleJdJSON', {
+        // Your request body here, if necessary
+      })
+      .then(response => {
+        setJobData(response.data.jdList);
+      })
+      .catch(error => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  if (!Array.isArray(jobData)) {
+    return <div>Data is not available or still loading.</div>;
+  }
+
   return (
-    <div className="App">
-      <JobsList jobs={jobs}/>
-    </div>
+    <div>
+      <JobList jobs={jobData} />
+  </div>
   );
 }
 
